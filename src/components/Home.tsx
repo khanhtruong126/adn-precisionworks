58ad6069abefd7ffa2804e477e962ab3ea559d47
Truong Nguyen <khanhtruong52@gmail.com>
2024-03-06 01:01:57 +0700

    edit home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,22 +6,16 @@ import { SECTION_ID } from "../App";
 import { apwRed, gray } from "../colors";
 import homeItem from "../assets/home-item.png";
 import AppLogo from "../assets/main-logo.svg";
+import { CaretRightFilled } from "@ant-design/icons";
 
-const { Title } = Typography;
 
 const Home = () => {
-  const handleClickScroll = () => {
-    const element = document.getElementById("about-us");
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
 
   return (
     <HomeContanier id={SECTION_ID.HOME}>
       <Row>
         <Col
-          lg={12}
+          lg={14}
           md={24}
           sm={24}
           className="flex mb-5 md:justify-center sm:justify-center"
@@ -32,45 +26,52 @@ const Home = () => {
               <Title level={4}>Contact Us</Title>
             </Button> */}
             <p>
-              <h2 className="mb-2">Reliable and Flexible</h2>
-              <span className="text-lg">
-                ADN Precision Works (APW), we are specialized in procurement of
-                machined parts, manufactured at local CNC shops located in Ho
-                Chi Minh and Ha Noi, Vietnam Our experienced and diversity
-                network allows us support customer in divert market with a
-                reliability result.
-                <br /> We try the best to create the products and service
-                solution that brings Values to customer <br />{" "}
+              <h3 className="mb-2">
+                Unlocking Vietnam's Precision: Your Gateway to Quality Machined
+                Parts
+              </h3>
+              <div className="text-lg text-justify">
+                <p className="mb-2">
+                  ADN Precision works (
+                  <span className="highlight-title">ADN</span>), your premier
+                  partner in procuring high-quality machined parts from the
+                  heart of Vietnam. With a commitment to excellence and a
+                  network of trusted manufacturing partners, we are your trusted
+                  source for precision-engineered components that meet and
+                  exceed industry standards.
+                </p>
+                <div className="flex">
+                  <CaretRightFilled />
+                  <span>
+                    We do service{" "}
+                    <span className="highlight-title">from prototype</span> to
+                    productions
+                  </span>
+                </div>
+                <div className="flex">
+                  <CaretRightFilled />
+                  <b>At ADN</b>, our responsible to delivery to customers
+                </div>
                 <div className="ml-5 mt-2">
                   <div className="flex gap-2">
                     <img src={AppLogo} alt="main_logo" width={25} height={25} />
-                    <h2>Low cost</h2>
+                    <h2>HIGH QUALITY PRODUCTS</h2>
                   </div>
                   <div className="flex gap-2">
                     <img src={AppLogo} alt="main_logo" width={25} height={25} />
-                    <h2>Short leadtime</h2>
+                    <h2>FASTER</h2>
                   </div>
                   <div className="flex gap-2">
                     <img src={AppLogo} alt="main_logo" width={25} height={25} />
-                    <h2>High performance products</h2>
+                    <h2>REASONABLE COST</h2>
                   </div>
                 </div>
-              </span>
+              </div>
             </p>
           </div>
         </Col>
-        <Col id="home-panel" lg={12} md={24} sm={24}></Col>
+        <Col id="home-panel" lg={10} md={24} sm={24}></Col>
       </Row>
-      {/* <div id="home-panel">
-        <div id="background-faded-overlay">
-          <div id="home-contact">
-            <Title>ADN Precision Works</Title>
-            <Button size="large" onClick={handleClickScroll}>
-              <Title level={4}>Contact Us</Title>
-            </Button>
-          </div>
-        </div>
-      </div> */}
     </HomeContanier>
   );
 };
